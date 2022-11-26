ba873fe873a806c4eb730ba671a37cd33e07812a
Adil Saju <adilsaju@gmail.com>
2022-11-25 22:54:46 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+
+
+const port = 5002
+
+
+// addData()
+// app.use(cors())
+
+//middleware
+app.use(express.json())
+// app.disable('view cache');
+
+//router 
+app.get("/", (req,res)=>{
+    res.send( 'hw' );
+})
+
+
+app.listen(port, ()=>{
+    console.log('server started');
+})
