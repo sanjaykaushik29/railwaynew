04e6b695e5a677eeff55b4c5c4ad6e32e67ea483
sanjaykaushik29 <sanjay@thoughtwin.com>
2023-08-04 18:44:19 +0530

    sanja

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 // Replace the hard-coded values with the env variables
 connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
-  // port: process.env.MYSQL_PORT,
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
