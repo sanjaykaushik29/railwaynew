a439e298f9f2d10105421973b5567ad47883575b
sanjaykaushik29 <sanjay@thoughtwin.com>
2023-08-07 12:11:59 +0530

    updated

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express();
 
-const { connection } = require("./db");
+// const { connection } = require("./db");
 
 
-const port =  5002
+const port = 5002
 
 
 // addData()
@@ -15,28 +15,28 @@ app.use(express.json())
 // app.disable('view cache');
 
 
-app.get("/api/get_users", (req, res) => {
-  connection.query("SELECT * FROM Users", (err, data) => {
-    if (err) return callback(err, null);
-    res.status(200).json({
-      status: "success",
-      length: data.length,
-      data,
-    });
-  });
-});
+// app.get("/api/get_users", (req, res) => {
+//   connection.query("SELECT * FROM Users", (err, data) => {
+//     if (err) return callback(err, null);
+//     res.status(200).json({
+//       status: "success",
+//       length: data.length,
+//       data,
+//     });
+//   });
+// });
 
 //router 
-app.get("/", (req,res)=>{
-    res.send( 'hw' );
+app.get("/", (req, res) => {
+  res.send('hw');
 })
 app.get("/api", (req, res) => {
 
-    res.send("serever is running ok").status(200)
-      });
-    
-    
+  res.send("serever is running ok").status(200)
+});
 
-app.listen(port, ()=>{
-    console.log('server started');
+
+
+app.listen(port, () => {
+  console.log('server started');
 })
